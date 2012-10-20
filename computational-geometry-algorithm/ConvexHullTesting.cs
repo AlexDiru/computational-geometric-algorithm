e45ae467128e5dd59ad92da01f4c7c0f19d7641e
Alexander Spedding <battleprogrammeralex@gmail.com>
2012-10-20 15:03:16 +0100

    Testing attempted

--- computational-geometry-algorithm/ConvexHullTesting.cs
+++ computational-geometry-algorithm/ConvexHullTesting.cs
@@ -10,6 +10,22 @@ namespace computational_geometry_algorithm
     /// </summary>
     public static class ConvexHullTesting
     {
+        public static List<Point2D> GenerateRandomPolygon(Int32 maxPointNumber, Int32 maxX, Int32 maxY)
+        {
+            //Create points
+            List<Point2D> pointList = new List<Point2D>();
+            for (int p = 0; p < maxPointNumber; p++)
+            {
+                Point2D newPoint = new Point2D(random.Next(maxX), random.Next(maxY));
+
+                if (!DataSet.Contains(pointList, newPoint))
+                    pointList.Add(newPoint);
+            }
+            pointList = pointList.Distinct().ToList();
+
+            return pointList;
+        }
+
         public static void RandomisedTests(Int32 testNumber, Int32 maxPointNumber, Int32 maxX, Int32 maxY)
         {
             Random random = new Random();
@@ -17,16 +33,7 @@ namespace computational_geometry_algorithm
             //Iterate tests
             for (int i = 0; i < testNumber; i++)
             {
-                //Create points
-                List<Point2D> pointList = new List<Point2D>();
-                for (int p = 0; p < maxPointNumber; p++)
-                {
-                    Point2D newPoint = new Point2D(random.Next(maxX), random.Next(maxY));
-
-                    if (!DataSet.Contains(pointList, newPoint))
-                        pointList.Add(newPoint);
-                }
-                pointList = pointList.Distinct().ToList();
+                var pointList = GenerateRandomPolygon(maxPointNumber, maxX, maxY);
 
                 Console.WriteLine(String.Format("Test #{0} | Point Number = {1}",i+1, pointList.Count));
                 UserInterface.Draw(pointList);
@@ -45,5 +52,26 @@ namespace computational_geometry_algorithm
             Console.WriteLine("Convex Hull | Point Number = " + pointList.Count);
             UserInterface.Draw(pointList);
         }
+
+        public static void PathPlannerSingleObstacleRandomisedTest(Int32 testNumber, Int32 maxPointNumber, Int32 maxX, Int32 maxY)
+        {
+            //Generate the map 
+            //Must have an empty top and bottom and left and right
+            String generatedMap = "";
+
+            Random random = new Random();
+
+            var polygon = GenerateRandomPolygon(maxPointNumber, maxX, maxY);
+            
+            //Insert polygon into generated Map
+            Int32 polygonWidth = polygon.Max(p => p.X);
+            Int32 polygonHeight = polygon.Max(p => p.Y);
+            Int32 mapWidth = polygonWidth + 2 + random.Next(10);
+            Int32 mapHeight = polygonHeight + 2 + random.Next(10);
+            Int32 leftOffset = mapWidth / 2;
+            Int32 topOffset = mapHeight / 2;
+
+            for (;;)
+        }   
     }
 }
